414c5708b5099a9e8be4a6712a7d8f6da55417da
Sean Westfall <sean.westfall@gmail.com>
2017-08-31 08:11:40 -0700

    2017-08 (#4)
    * Updated cards with second sponsor and added comments to help know which slide is which; also blanked out second speaker on next.js
    
    * Updated the size of edmunds sponsorship image, also the size of the first speakers profile photo
    
    * Fixed a spacing issue show in the travis CI build logs
    
    * Fixed testing issues in next.js

--- client/cards.js
+++ client/cards.js
@@ -1,19 +1,19 @@
 module.exports = [
-  require('./cards/coc'),
-  require('./cards/drinks'),
-  require('./cards/sponsor1'),
-  require('./cards/speaker1'),
-  require('./cards/nodeschool'),
+  require('./cards/coc'),             // 0
+  require('./cards/drinks'),          // 1
+  require('./cards/sponsor1'),        // 2
+  require('./cards/speaker1'),        // 3
+  require('./cards/nodeschool'),      // 4
 
-  require('./cards/lunch'),
-  require('./cards/speakers-wanted'),
-  // require('./cards/sponsor2'),
-  require('./cards/speaker2'),
-  require('./cards/next'),
+  require('./cards/lunch'),           // 5
+  require('./cards/speakers-wanted'), // 6
+  require('./cards/sponsor2'),        // 7
+  require('./cards/speaker2'),        // 8
+  require('./cards/next'),            // 9
 
-  require('./cards/youtube'),
-  require('./cards/sponsors-wanted'),
-  require('./cards/slack'),
-  require('./cards/volunteer'),
-  require('./cards/fullstack-react')
+  require('./cards/youtube'),         // 10
+  require('./cards/sponsors-wanted'), // 11
+  require('./cards/slack'),           // 12
+  require('./cards/volunteer'),       // 13
+  require('./cards/fullstack-react')  // 14
 ]

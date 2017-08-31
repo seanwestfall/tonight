9f74afea10de092f4c77d09cb199d02218c44baf
seanwestfall <sean.westfall@gmail.com>
2017-08-31 00:40:11 -0700

    Updated the size of edmunds sponsorship image, also the size of the first speakers profile photo

--- client/cards.js
+++ client/cards.js
@@ -1,19 +1,19 @@
 module.exports = [
-  require('./cards/coc'),             //1
-  require('./cards/drinks'),          //2
-  require('./cards/sponsor1'),        //3
-  require('./cards/speaker1'),        //4
-  require('./cards/nodeschool'),      //5
+  require('./cards/coc'),             //0
+  require('./cards/drinks'),          //1
+  require('./cards/sponsor1'),        //2
+  require('./cards/speaker1'),        //3
+  require('./cards/nodeschool'),      //4
 
-  require('./cards/lunch'),           //6
-  require('./cards/speakers-wanted'), //7
-  require('./cards/sponsor2'),        //8
-  require('./cards/speaker2'),        //9
-  require('./cards/next'),            //10
+  require('./cards/lunch'),           //5
+  require('./cards/speakers-wanted'), //6
+  require('./cards/sponsor2'),        //7
+  require('./cards/speaker2'),        //8
+  require('./cards/next'),            //9
 
-  require('./cards/youtube'),         //11
-  require('./cards/sponsors-wanted'), //12
-  require('./cards/slack'),           //13
-  require('./cards/volunteer'),       //14
-  require('./cards/fullstack-react')  //15
+  require('./cards/youtube'),         //10
+  require('./cards/sponsors-wanted'), //11
+  require('./cards/slack'),           //12
+  require('./cards/volunteer'),       //13
+  require('./cards/fullstack-react')  //14
 ]
--- client/cards/speaker1.js
+++ client/cards/speaker1.js
@@ -4,5 +4,5 @@ module.exports = h('.speaker',
   h('h1', 'Why you should (almost) always use stateful components in React'),
   h('h2', 'Aaron Craig'),
 
-  h('img', {src: '/img/speakers/aaron-craig.jpeg', style: {width: '50%'}})
+  h('img', {src: '/img/speakers/aaron-craig.jpeg', style: {width: '25%'}})
 )

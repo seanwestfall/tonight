db1a016370cf3d2c4dd1218fa6e1965a1b3c8832
seanwestfall <sean.westfall@gmail.com>
2017-08-31 00:03:39 -0700

    Updated cards with second sponsor and added comments to help know which slide is which; also blanked out second speaker on next.js

--- client/cards.js
+++ client/cards.js
@@ -1,19 +1,19 @@
 module.exports = [
-  require('./cards/coc'),
-  require('./cards/drinks'),
-  require('./cards/sponsor1'),
-  require('./cards/speaker1'),
-  require('./cards/nodeschool'),
+  require('./cards/coc'),             //1
+  require('./cards/drinks'),          //2
+  require('./cards/sponsor1'),        //3
+  require('./cards/speaker1'),        //4
+  require('./cards/nodeschool'),      //5
 
-  require('./cards/lunch'),
-  require('./cards/speakers-wanted'),
-  // require('./cards/sponsor2'),
-  require('./cards/speaker2'),
-  require('./cards/next'),
+  require('./cards/lunch'),           //6
+  require('./cards/speakers-wanted'), //7
+  require('./cards/sponsor2'),        //8
+  require('./cards/speaker2'),        //9
+  require('./cards/next'),            //10
 
-  require('./cards/youtube'),
-  require('./cards/sponsors-wanted'),
-  require('./cards/slack'),
-  require('./cards/volunteer'),
-  require('./cards/fullstack-react')
+  require('./cards/youtube'),         //11
+  require('./cards/sponsors-wanted'), //12
+  require('./cards/slack'),           //13
+  require('./cards/volunteer'),       //14
+  require('./cards/fullstack-react')  //15
 ]
